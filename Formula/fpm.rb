7ebb44284554bd65f75b04e82648fa389cc7c29d
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-03-29 19:17:18 +0000

    fpm: add 0.5.0 bottle.

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -5,6 +5,12 @@ class Fpm < Formula
   sha256 "e4a06956d2300f9aa1d06bd3323670480e946549617582e32684ded6921a921e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.5.0"
+    sha256 cellar: :any, big_sur:  "e8f5393d340e4c4230c8ce207069ccb4d797868b7e38a9ebe77c4084dad6e53c"
+    sha256 cellar: :any, catalina: "9d6174df624b83d6b878ab1352d19c68dc0bc3e70c12cfb7927151dc7997e52b"
+  end
+
   depends_on "curl" => :build
   depends_on "gcc" # for gfortran
   fails_with gcc: "4"
