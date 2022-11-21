bc0b2055b57e0cc5d6dab153ee0e90c23e6c24e6
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-11-21 06:07:33 +0000

    fpm: update 0.7.0 bottle.

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -6,9 +6,9 @@ class Fpm < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.6.0_1"
-    sha256 cellar: :any, monterey: "e7dd3da88e6823a2056a8f2f705c00d96b996fea331e16d4dbd42e365736a36e"
-    sha256 cellar: :any, big_sur:  "be32d0318c31dd6e759347804a7c40ae075599f25b0fcc3b663afd6c9852faeb"
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.7.0"
+    sha256 cellar: :any, monterey: "108def5ea47566a982c910ef0b1c893df85797685cf8264336d2799c813905f4"
+    sha256 cellar: :any, big_sur:  "53f38db60b36ba6e21637b6ba15d92040d807a26c517e264a8025eb24109d662"
   end
 
   depends_on "curl" => :build
