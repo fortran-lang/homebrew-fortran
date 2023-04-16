7a2845dc64852720c2d5e6dcf739d554b4aa5d3e
Federico Perini <federico.perini@gmail.com>
2023-04-16 10:29:06 +0200

    remove 0.7.0 bottles

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -5,12 +5,6 @@ class Fpm < Formula
   sha256 "0bd978bb1d3f2a3297d82a0d6ac009746a466cfa9a59ba3b6513b74e5ce4b7bf"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.7.0"
-    sha256 cellar: :any, monterey: "108def5ea47566a982c910ef0b1c893df85797685cf8264336d2799c813905f4"
-    sha256 cellar: :any, big_sur:  "53f38db60b36ba6e21637b6ba15d92040d807a26c517e264a8025eb24109d662"
-  end
-
   depends_on "curl" => :build
   depends_on "gcc" # for gfortran
   fails_with gcc: "4"
