79001539df2899b8601f64ad77cb0c3a1927fbdb
Federico Perini <federico.perini@gmail.com>
2023-04-29 22:35:44 +0200

    2-space indent

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -6,9 +6,9 @@ class Fpm < Formula
   license "MIT"
 
   bottle do
-     root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.7.0"
-     sha256 cellar: :any, monterey: "108def5ea47566a982c910ef0b1c893df85797685cf8264336d2799c813905f4"
-     sha256 cellar: :any, big_sur:  "53f38db60b36ba6e21637b6ba15d92040d807a26c517e264a8025eb24109d662"
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.7.0"
+    sha256 cellar: :any, monterey: "108def5ea47566a982c910ef0b1c893df85797685cf8264336d2799c813905f4"
+    sha256 cellar: :any, big_sur:  "53f38db60b36ba6e21637b6ba15d92040d807a26c517e264a8025eb24109d662"
   end
 
   depends_on "curl" => :build
