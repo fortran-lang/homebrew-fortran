d25088ec51571d7a3d532fedfdeeeb4f7c13122b
Federico Perini <federico.perini@gmail.com>
2023-05-02 13:35:28 +0200

    quotes

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -23,7 +23,7 @@ class Fpm < Formula
 
   def install
     ENV.fortran
-    ENV[HOMEBREW_NO_INSTALL_FROM_API] = "1"
+    ENV["HOMEBREW_NO_INSTALL_FROM_API"] = "1"
     chmod("a+x", "./install.sh")
     system "./install.sh", "--prefix=#{prefix}"
   end
