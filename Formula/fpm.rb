47703378222a02d259ce4c2ce53028268d9e2e58
Federico Perini <11460684+perazz@users.noreply.github.com>
2023-05-07 14:43:45 +0000

    fpm: update 0.8.1 bottle.

--- Formula/fpm.rb
+++ Formula/fpm.rb
@@ -6,9 +6,9 @@ class Fpm < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.7.0"
-    sha256 cellar: :any, monterey: "108def5ea47566a982c910ef0b1c893df85797685cf8264336d2799c813905f4"
-    sha256 cellar: :any, big_sur:  "53f38db60b36ba6e21637b6ba15d92040d807a26c517e264a8025eb24109d662"
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/fpm-0.8.1"
+    sha256 cellar: :any, ventura:  "8d2cc3dea7e1fd64f760814c2cee235af3ae196747a76817c375421b5022dc81"
+    sha256 cellar: :any, monterey: "d9faf79766a21dba27c009c335a2ab8b65dc6e3b8131c8953d7d27d240945232"
   end
 
   depends_on "curl" => :build
