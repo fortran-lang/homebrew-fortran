d6fae98b46f479078d8622c0781912c469d08826
Gauthier Segay <gauthier.segay@gmail.com>
2025-03-01 14:02:01 +0100

    bump lfortran formula.

--- Formula/lfortran.rb
+++ Formula/lfortran.rb
@@ -1,18 +1,18 @@
 class Lfortran < Formula
   desc "Modern interactive LLVM-based Fortran compiler"
   homepage "https://lfortran.org"
-  url "https://lfortran.github.io/tarballs/release/lfortran-0.16.0.tar.gz"
-  sha256 "9e1ae952bdaee845d9b9318ea06ed199c74da26704926c20fadaab794307c4fe"
+  url "https://lfortran.github.io/tarballs/release/lfortran-0.46.0.tar.gz"
+  sha256 "420885b4bcfd2206bc5ae9cdb658f2382b35b0e4d145476a97bdc7d642d4a588"
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/lfortran-0.16.0"
-    sha256 cellar: :any, big_sur: "67d1ee4fd768f602cd2ed2dcd26dce2b2b011edaebbb7fea0e91541d4384624e"
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/lfortran-0.46.0"
+    # sha256 cellar: :any, big_sur: "67d1ee4fd768f602cd2ed2dcd26dce2b2b011edaebbb7fea0e91541d4384624e"
   end
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  depends_on "llvm@11"
+  depends_on "llvm"
   depends_on "zlib"
 
   def install
@@ -26,14 +26,14 @@ class Lfortran < Formula
   end
 
   test do
-    system "#{bin}/lfortran", "--version"
+    system "bin/lfortran", "--version"
     (testpath/"hello.f90").write <<~EOS
       program hello
         print *, "Hello, World!"
       end
     EOS
-    system "#{bin}/lfortran", testpath/"hello.f90", "-o", testpath/"hello"
-    assert_predicate testpath/"hello", :exist?
+    system "bin/lfortran", testpath/"hello.f90", "-o", testpath/"hello"
+    assert_path_exists testpath/"hello"
     system testpath/"hello"
   end
 end
