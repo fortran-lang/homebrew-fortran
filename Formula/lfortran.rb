b7bcfce0fb0b65dede31fd1bd0b8a641c8820a35
Federico Perini <federico.perini@gmail.com>
2025-03-12 19:01:06 +0100

    lfortran 0.48.0

--- Formula/lfortran.rb
+++ Formula/lfortran.rb
@@ -1,12 +1,12 @@
 class Lfortran < Formula
   desc "Modern interactive LLVM-based Fortran compiler"
   homepage "https://lfortran.org"
-  url "https://lfortran.github.io/tarballs/release/lfortran-0.46.0.tar.gz"
+  url "https://github.com/lfortran/lfortran/releases/download/v0.48.0/lfortran-0.48.0.tar.gz"
   sha256 "420885b4bcfd2206bc5ae9cdb658f2382b35b0e4d145476a97bdc7d642d4a588"
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/lfortran-0.46.0"
+    root_url "https://github.com/fortran-lang/homebrew-fortran/releases/download/lfortran-0.48.0"
     rebuild 1
     sha256 cellar: :any, arm64_sonoma: "18463218cdd1a5fc09b468f27051b55a03c8c693c73aff6b3a6696cc439c2f59"
     sha256 cellar: :any, ventura:      "62dc18247373cd3ec6364783e8e3e645d246e64b8a72026a3be6eca3f892f227"
@@ -22,6 +22,7 @@ class Lfortran < Formula
     cmake_args = std_cmake_args
     cmake_args << "-DCMAKE_CXX_FLAGS_RELEASE=-O3 -funroll-loops -DNDEBUG"
     cmake_args << "-DWITH_LLVM=ON"
+    cmake_args << "-DWITH_LSP=yes"
     system "cmake", *cmake_args, "-G", "Ninja", "-B", "build"
     system "cmake", "--build", "build"
     system "ctest", "--test-dir", "build", "--output-on-failure"
